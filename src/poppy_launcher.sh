c4a870bb04c21e0068e901aae61584c3d1ab4b1b
Nicolas <nicolas.rabault@inria.fr>
2015-03-17 11:46:38 +0100

    try to change user for webapp installer

--- src/poppy_launcher.sh
+++ src/poppy_launcher.sh
@@ -37,7 +37,7 @@ export HOME=/home/poppy
 su - poppy -c "curl -L https://raw.githubusercontent.com/pierre-rouanet/build-raspbian-image/master/delivery/poppy-installer | bash -s poppy-humanoid" >> /home/poppy/install_log
 
 # WebApp installer
-curl -L https://raw.githubusercontent.com/MakingBot/webapp/master/poppy/setup.sh | bash >> /home/poppy/install_log
+su - poppy -c "curl -L https://raw.githubusercontent.com/MakingBot/webapp/master/poppy/setup.sh | sudo bash" >> /home/poppy/install_log
 
 # Remove instalation at startup
 sed -i /install_log/d /home/poppy/.bashrc
